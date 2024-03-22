57dbae77b8fb8c95383182be85737f1209ae1984
Teerapong Boonkwan <Teerapongb.work@gmail.com>
2024-03-22 14:51:54 +0700

    [Feed] Enable kotlin.parcelize

--- domain/src/main/java/com/primo/domain/entity/FeedUIModel.kt
+++ domain/src/main/java/com/primo/domain/entity/FeedUIModel.kt
@@ -1,11 +1,17 @@
 package com.primo.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
 data class FeedUIModel(
     var title: String = "",
     var logo: String = "",
     var feedList: List<FeedDetail> = listOf()
-)
+) : Parcelable
 
+@Parcelize
 data class FeedDetail(
     var image: String,
     val title: String,
@@ -14,4 +20,4 @@ data class FeedDetail(
     var categories: List<String>,
     var link: String,
     var author: String
-)
+) : Parcelable
